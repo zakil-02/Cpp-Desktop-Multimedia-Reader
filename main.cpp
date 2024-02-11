7c379c06573b230f66cff255e5aa35aa0fbabd8a
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2024-02-12 00:40:35 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// main.cpp
+// Created on 21/10/2018
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+    std::cout << "Hello brave new world popopoop" << std::endl;
+    return 0;
+}
