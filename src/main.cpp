15b326746dea5a5e2cc13e792330446e9263cbf0
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2024-07-14 14:47:16 +0200

    Multimedia uniform processing

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <memory>
 #include "..\include\multimediaObject.h"
 #include "..\include\photoObject.h"
 #include "..\include\videoObject.h"
@@ -14,24 +16,26 @@ using namespace std;
 int main(int argc, const char* argv[])
 {
     cout << "Test" << std::endl;
+    cout << "----------------" << std::endl;
     /*
     cout << "----------------" << std::endl;
 
     MultimediaObject* obj = new MultimediaObject("Object1", "Path1");
     obj->afficher(cout);
     */
-    cout << "----------------" << std::endl;
+
+    vector<shared_ptr<MultimediaObject>> mediaList;
 
     Photo photo("Mee", "C:/Users/akilh/OneDrive/Bureau/TELECOM_2A/UE_Electives_crD/INF224-ParadigmesDeProgrammation/pdp.jpg", 48.8584, 2.2945);
-    cout << "Photo object:" << endl;
-    photo.afficher(cout);
-    photo.play("windows");
-
-    cout << "----------------" << std::endl;
-    
     Video video("Street Video", "C:/Users/akilh/OneDrive/Bureau/TELECOM_2A/UE_Electives_crD/INF224-ParadigmesDeProgrammation/street.mp4", 120);
-    cout << "Video object:" << endl;
-    video.afficher(cout);
-    video.play("windows");
+
+    mediaList.push_back(make_shared<Photo>(photo));
+    mediaList.push_back(make_shared<Video>(video));
+
+    for (auto& media : mediaList) {
+        cout << "----------------" << endl;
+        media->afficher(cout);
+        media->play("windows");
+    }
     return 0;
 }
